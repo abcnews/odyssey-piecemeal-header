a3d720c9c01bd7fd5ea4e737284d3230b53898b8
Colin Gourlay <Gourlay.Colin@abc.net.au>
2018-12-07 14:28:22 +1000

    Retain original Header, displaying it on smaller viewports. Fix interplay with global nav.

--- src/index.js
+++ src/index.js
@@ -9,14 +9,11 @@ function init() {
 
   headerBlockContentEl.innerHTML = '';
   headerBlockContentEl.className = `${headerBlockContentEl.className} ${styles.root}`;
-  headerEl.parentElement.removeChild(headerEl);
   Array.prototype.slice
     .call(headerContentEl.childNodes)
     .forEach(node => node.tagName !== 'DIV' && headerBlockContentEl.appendChild(node.cloneNode(true)));
+
   firstBlockContentEl.parentElement.insertBefore(headerBlockContentEl, firstBlockContentEl);
-  document.documentElement.classList.add(styles.hasOdysseyPiecemealHeader);
 }
 
-if (window.matchMedia('(min-width: 61.25rem)').matches) {
-  window.__ODYSSEY__ ? init() : window.addEventListener('odyssey:api', init);
-}
+window.__ODYSSEY__ ? init() : window.addEventListener('odyssey:api', init);
